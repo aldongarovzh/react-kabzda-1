6e88c1c3660cb5535673cab243076263099ac041
Zhambyl Aldongarov <aldongarovzh@gmail.com>
2022-07-22 04:54:57 +0600

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return  <header className="header">
+                <img src="https://i.pinimg.com/originals/98/e3/f8/98e3f8fbdac57fef3d6d239da334e924.png" alt=""/>
+            </header>
+};
+
+export default Header;
---
+++ components/Profile.jsx
@@ -0,0 +1,26 @@
+
+
+const Profile = () => {
+    return <div className="content">
+                <div>
+                    <img src="https://pix10.agoda.net/hotelImages/301716/-1/fe9724d8fb4da3dd4590353bd771a276.jpg?ca=9&ce=1&s=1024x768" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+};
+
+export default Profile;
